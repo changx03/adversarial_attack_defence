72408b4373f890a9c02991f959aa326c6b43339f
Luke Chang <luke.x.chang@gmail.com>
2020-03-12 18:40:09 +1300

    test

--- main.py
+++ main.py
@@ -17,35 +17,21 @@ def main():
 
     DATA_ROOT = 'data'
     BATCH_SIZE = 64
-    TYPE = 'quantitative'  # image or quantitative
+    TYPE = 'image'  # image or quantitative
     # image: 'MNIST', 'CIFAR10', 'SVHN'
     # quantitative: 'BankNote', 'BreastCancerWisconsin', 'WheatSeed', 'HTRU2'
-    NAME = 'HTRU2'
+    NAME = 'MNIST'
 
     print(f'Start {NAME} data container')
     IMAGE_DATASET = DATASET_LIST[TYPE][NAME]
-    data_container = DataContainer(IMAGE_DATASET, DATA_ROOT)
-    data_container(batch_size=BATCH_SIZE, normalize=False)
-    print(len(data_container))
-    print('train', data_container.dim_train)
-    print(data_container.label_test_np[:16])
+    dc = DataContainer(IMAGE_DATASET, DATA_ROOT)
+    dc(batch_size=BATCH_SIZE, normalize=False)
+    print(len(dc))
+    print('train:', dc.dim_train)
+    print('test[:16]: ', dc.label_test_np[:16])
+    print('train mean:', dc.train_mean)
+    print('train std:', dc.train_std)
 
-    print('Compute mean and std from train set')
-    data_np = data_container.data_train_np
-    print('mean: [', end='')
-    for m in data_np.mean(axis=0):
-        print(f'{m:.4f}', end=', ')
-    print('],')
-    print('std: [', end='')
-    for s in data_np.std(axis=0):
-        print(f'{s:.4f}', end=', ')
-    print('],')
-
-    print(data_container.train_mean)
-    print(data_container.train_std)
-
-    print(data_container.dataframe.head())
-    
     # model = ModelContainer()
     # model()
     # attack = AttackContainer()
