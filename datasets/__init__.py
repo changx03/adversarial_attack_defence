fd205adbef6a8b0aed7d31dc04df5479e17a198b
Luke Chang <luke.x.chang@gmail.com>
2020-03-14 23:41:12 +1300

    minor fix

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -7,7 +7,11 @@ from .utils import (get_range, scale_normalize, scale_unnormalize,
                     shuffle_data, swap_image_channel)
 
 __all__ = [
-    'DataContainer', 'DATASET_LIST', 'get_range', 'scale_normalize', 'scale_unnormalize'
-    'shuffle_data', 'NumeralDataset', 'get_image_list', 'get_quantitative_list',
-    'MEAN_LOOKUP', 'STD_LOOKUP', 'get_sample_mean', 'get_sample_std', 'swap_image_channel'
+    'DataContainer',
+    'DATASET_LIST', 'MEAN_LOOKUP', 'STD_LOOKUP',
+    'get_image_list', 'get_quantitative_list',
+    'get_sample_mean', 'get_sample_std',
+    'NumeralDataset',
+    'get_range', 'scale_normalize', 'scale_unnormalize',
+    'shuffle_data', 'swap_image_channel'
 ]
