9c21374b8b4ae0d75bd298ee93bb4e11c65dfeaa
Luke Chang <luke.x.chang@gmail.com>
2020-03-16 02:35:37 +1300

    added deep fool attack

--- attacks/DeepFoolContainer.py
+++ attacks/DeepFoolContainer.py
@@ -9,12 +9,55 @@ from .AttackContainer import AttackContainer
 
 
 class DeepFoolContainer(AttackContainer):
-    def __init__(self, model_container):
-        super(DeepFoolContainer, self).__init__(model_container)
+    def __init__(self, model_container, max_iter=100, epsilon=1e-6,
+                 nb_grads=10, batch_size=1):
+        super(DeepFoolContainer, self).__init__(model_container, )
 
-        # TODO: implement this
-        raise NotImplementedError
+        params_received = {
+            'max_iter': max_iter,
+            'epsilon': epsilon,
+            'nb_grads': nb_grads,
+            'batch_size': batch_size
+        }
+        self.attack_params.update(params_received)
 
-    def generate(self):
-        # TODO: implement this
-        raise NotImplementedError
+        # use IBM ART pytorch module wrapper
+        # the model used here should be already trained
+        model = self.model_container.model
+        loss_fn = self.model_container.model.loss_fn
+        clip_values = self.model_container.data_container.data_range
+        optimizer = self.model_container.model.optimizer
+        num_classes = self.model_container.data_container.num_classes
+        dim_data = self.model_container.data_container.dim_data
+        self.classifier = PyTorchClassifier(
+            model=model,
+            clip_values=clip_values,
+            loss=loss_fn,
+            optimizer=optimizer,
+            input_shape=dim_data,
+            nb_classes=num_classes)
+
+    def generate(self, count=1000, use_testset=True, x=None, **kwargs):
+        assert use_testset or x is not None
+
+        since = time.time()
+        # parameters should able to set before training
+        self.set_params(**kwargs)
+
+        dc = self.model_container.data_container
+        # handle the situation where testset has less samples than we want
+        if use_testset and len(dc.data_test_np) < count:
+            count = len(dc.data_test_np)
+
+        x = np.copy(dc.data_test_np[:count]) if use_testset else np.copy(x)
+
+        attack = DeepFool(self.classifier, **self.attack_params)
+
+        # predict the outcomes
+        adv = attack.generate(x)
+        y_adv, y_clean = self.predict(adv, x)
+
+        time_elapsed = time.time() - since
+        print('Time taken for training {} adversarial examples: {:2.0f}m {:2.1f}s'.format(
+            count, time_elapsed // 60, time_elapsed % 60))
+        return adv, y_adv, np.copy(x), y_clean
--- attacks/__init__.py
+++ attacks/__init__.py
@@ -1,11 +1,13 @@
 from .AttackContainer import AttackContainer
 from .BIMContainer import BIMContainer
 from .CarliniL2Container import CarliniL2Container
+from .DeepFoolContainer import DeepFoolContainer
 from .FGSMContainer import FGSMContainer
 
 __all__ = [
     'AttackContainer',
     'BIMContainer',
     'CarliniL2Container',
+    'DeepFoolContainer',
     'FGSMContainer',
 ]
