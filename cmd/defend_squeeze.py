06634a59bef2fd2a110abd5f75fc4e73e06a73ed
Luke Chang <luke.x.chang@gmail.com>
2020-04-21 13:27:04 +1200

    working on script for feature squeeze

---
+++ cmd/defend_squeeze.py
@@ -0,0 +1,167 @@
+import argparse as ap
+import logging
+import os
+
+import numpy as np
+
+from aad.attacks import BIMContainer
+from aad.basemodels import IrisNN, ModelContainerPT, get_model
+from aad.defences import FeatureSqueezing
+from aad.utils import get_time_str, master_seed
+from cmd_utils import get_data_container, parse_model_filename, set_logging
+
+LOG_NAME = 'DefSqueeze'
+logger = logging.getLogger(LOG_NAME)
+
+
+def main():
+    parser = ap.ArgumentParser()
+    parser.add_argument(
+        '-m', '--model', type=str, required=True,
+        help='a file which contains a pretrained model. The filename should in "<model>_<dataset>_e<max epochs>[_<date>].pt" format')
+    parser.add_argument(
+        '-b', '--bitdepth', type=int, required=True,
+        help='The image color depth for input images')
+    parser.add_argument(
+        '-s', '--sigma', type=int, required=True,
+        help='The Standard Deviation of normal distribution')
+    parser.add_argument(
+        '-k', '--kernelsize', type=int, required=True,
+        help='The kernel size for median filter')
+    parser.add_argument(
+        '-M', '--median', action='store_true', default=False,
+        help='Apply median filter')
+    parser.add_argument(
+        '-D', '--depth', action='store_true', default=False,
+        help='Apply binary-depth filter')
+    parser.add_argument(
+        '-N', '--normal', action='store_true', default=False,
+        help='Apply Gaussian noise filter')
+    parser.add_argument(
+        '-s', '--seed', type=int, default=4096,
+        help='the seed for random number generator')
+    parser.add_argument(
+        '-v', '--verbose', action='store_true', default=False,
+        help='set logger level to debug')
+    parser.add_argument(
+        '-l', '--savelog', action='store_true', default=False,
+        help='save logging file')
+    parser.add_argument(
+        '-B', '--bim', action='store_true', default=False,
+        help='Apply BIM attack')
+    parser.add_argument(
+        '-C', '--carlini', action='store_true', default=False,
+        help='Apply Carlini L2 attack')
+    parser.add_argument(
+        '-D', '--deepfool', action='store_true', default=False,
+        help='Apply DeepFool attack')
+    parser.add_argument(
+        '-F', '--fgsm', action='store_true', default=False,
+        help='Apply FGSM attack')
+    parser.add_argument(
+        '-S', '--saliency', action='store_true', default=False,
+        help='Apply Saliency Map attack')
+    args = parser.parse_args()
+    model_file = args.model
+    bit_depth = args.bitdepth
+    sigma = args.sigma
+    kernel_size = args.kernelsize
+    seed = args.seed
+    verbose = args.verbose
+    save_log = args.savelog
+
+    model_name, data_name = parse_model_filename(model_file)
+
+    # Which filter should apply?
+    filter_list = []
+    if args.median:
+        filter_list.append('median')
+    if args.depth:
+        filter_list.append('binary')
+    if args.normal:
+        filter_list.append('normal')
+
+    # Which attack should apply?
+    attack_list = []
+    if args.bim:
+        attack_list.append('BIM')
+    if args.carlini:
+        attack_list.append('Carlini')
+    if args.deepfool:
+        attack_list.append('DeepFool')
+    if args.fgsm:
+        attack_list.append('FGSM')
+    if args.saliency:
+        attack_list.append('Saliency')
+
+    y_file = os.path.join(
+        'save', f'{model_name}_{data_name}_{attack_list[0]}_y.npy')
+    attack_files = [
+        os.path.join(
+            'save', f'{model_name}_{data_name}_{attack_list[0]}_x.npy')
+    ]
+    for attack_name in attack_list:
+        attack_files.append(os.path.join(
+            'save', f'{model_name}_{data_name}_{attack_name}_adv.npy'))
+    # the 1st file this the clean inputs
+    attack_list = ['clean'] + attack_list
+
+    need_train = False
+    pretrain_file = f'AdvTrain_{model_name}_{data_name}.pt'
+    if not os.path.exists(os.path.join('save', pretrain_file)):
+        need_train = True
+
+    # set logging config. Run this before logging anything!
+    set_logging(LOG_NAME, data_name, verbose, save_log)
+
+    # show parameters
+    print(f'[{LOG_NAME}] Running feature squeezing on {model_name}...')
+    logger.info('Start at    : %s', get_time_str())
+    logger.info('RECEIVED PARAMETERS:')
+    logger.info('model file  :%s', model_file)
+    logger.info('model       :%s', model_name)
+    logger.info('dataset     :%s', data_name)
+    logger.info('bit_depth  :%d', bit_depth)
+    logger.info('sigma  :%d', sigma)
+    logger.info('kernel_size  :%d', kernel_size)
+    logger.info('seed        :%d', seed)
+    logger.info('verbose     :%r', verbose)
+    logger.info('save_log    :%r', save_log)
+    logger.info('need train  :%r', need_train)
+    logger.info('filters     :%s', ', '.join(filter_list))
+    logger.info('attacks     :%s', ', '.join(attack_list))
+
+    # check files
+    for file_name in [model_file, y_file] + attack_files:
+        if not os.path.exists(file_name):
+            logger.error('%s does not exist!', file_name)
+            raise FileNotFoundError('{} does not exist!'.format(file_name))
+
+    # reset seed
+    master_seed(seed)
+
+    # select data
+    dc = get_data_container(
+        data_name,
+        use_shuffle=True,
+        use_normalize=True,
+    )
+
+    # select a model
+    Model = get_model(model_name)
+    model = Model()
+    if data_name in ('BankNote', 'HTRU2', 'Iris', 'WheatSeed'):
+        num_classes = dc.num_classes
+        num_features = dc.dim_data[0]
+        model = IrisNN(
+            num_features=num_features,
+            hidden_nodes=num_features*4,
+            num_classes=num_classes)
+    classifier_mc = ModelContainerPT(model, dc)
+    classifier_mc.load(model_file)
+    accuracy = classifier_mc.evaluate(dc.x_test, dc.y_test)
+    logger.info('Accuracy on test set: %f', accuracy)
+
+if __name__ == '__main__':
+    main()
+    print(f'[{LOG_NAME}] Task completed!')
