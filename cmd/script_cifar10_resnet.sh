102def8cc26e72b0ae72bedb252fbf4cc5df1d35
Luke Chang <luke.x.chang@gmail.com>
2020-05-21 02:12:33 +1200

    added terminal script

---
+++ cmd/script_cifar10_resnet.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# chmod -x ./cmd/script_cifar10_resnet.sh
+
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
+python ./aad/cmd/multi_image.py -vl -i 1 -e 50 -d CIFAR10 -m CifarResnet50
---
+++ cmd/script_svhn_resnet.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# chmod +x ./cmd/script_svhn_resnet.sh
+
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
+python ./cmd/multi_image.py -vl -i 1 -e 50 -d SVHN -m CifarResnet50
